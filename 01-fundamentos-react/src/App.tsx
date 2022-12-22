aff3567acd7f5f2e2d8033f34bcae4d883432da1
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-12-22 15:46:47 -0300

    #1 feat: stylize app

--- 01-fundamentos-react/src/App.tsx
+++ 01-fundamentos-react/src/App.tsx
@@ -2,11 +2,17 @@ import { Header } from './components/Header'
 
 import './global.css'
 
+import styles from './App.module.css'
+
 export function App() {
   return (
     <>
       <Header/>
-      <h1>Hello World</h1>
+      
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>posts</main>
+      </div>
     </>
   )
 }
