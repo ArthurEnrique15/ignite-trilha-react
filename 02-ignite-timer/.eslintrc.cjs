035f125cca561369221cc167a50f5d31f3377780
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-03 20:58:21 -0300

    chore: add eslint and rocketseat eslint config

--- 02-ignite-timer/.eslintrc.cjs
+++ 02-ignite-timer/.eslintrc.cjs
@@ -1,24 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  overrides: [
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
+  extends: ['@rocketseat/eslint-config/react'],
 }
