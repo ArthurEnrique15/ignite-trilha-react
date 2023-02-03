7abfe98a8c971e68512edb9c64ba26f76fff218a
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-03 18:58:06 -0300

    #1 refactor: set alt as a property of avatar

--- 01-fundamentos-react/src/components/Comment.tsx
+++ 01-fundamentos-react/src/components/Comment.tsx
@@ -23,7 +23,7 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/ianlgnk.png"/>
+      <Avatar hasBorder={false} src="https://github.com/ianlgnk.png" alt=""/>
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
