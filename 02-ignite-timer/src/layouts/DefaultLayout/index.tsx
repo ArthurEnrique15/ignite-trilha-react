9422a181f9647d9af0b23eabf77ce07859db190e
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-07 18:49:46 -0300

    style: stylize default layout

--- 02-ignite-timer/src/layouts/DefaultLayout/index.tsx
+++ 02-ignite-timer/src/layouts/DefaultLayout/index.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+
+import { LayoutContainer } from './styles'
 
 // Outlet é um espaço para ser inserido o conteúdo das rotas
 // assim, o conteúdo específico da página será inserido no componente DefaultLayout
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
