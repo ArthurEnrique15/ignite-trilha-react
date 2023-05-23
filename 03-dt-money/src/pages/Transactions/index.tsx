7ad4c17bb88f4d7dbf0cf9cb3dcc53e7834e64ae
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-05-22 22:07:51 -0300

    chore: move searchForm to inside transactions page

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { Header } from '../../components/Header'
-import { SearchForm } from '../../components/SearchForm'
+import { SearchForm } from './components/SearchForm'
 import { Summary } from '../../components/Summary'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 import { dateFormatter, priceFormatter } from '../../utils/formatter'
